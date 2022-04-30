918dfaf0675a0fa984ab43601a14abe5fd9b05c9
MikhailTheo <theo.vigneron@live.fr>
2022-04-30 20:56:13 +0200

    Exo 3

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,7 +1,13 @@
 package com.acme.todolist;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.acme.todolist.domain.TodoItem;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootTest
 class TodolistApplicationTests {
@@ -9,5 +15,19 @@ class TodolistApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+    public void isLateTrueTest(){
+    	//System.out.println(Instant.now().minus(10, ChronoUnit.DAYS));
+        TodoItem todoItem = new TodoItem("test", Instant.now().minus(10, ChronoUnit.DAYS),"test");
+        Assertions.assertTrue(todoItem.isLate());
+    }
+
+    @Test
+    void isLateFalseTest(){
+    	//System.out.println(Instant.now().plus(10, ChronoUnit.DAYS));
+        TodoItem todoItem = new TodoItem("test", Instant.now().plus(10, ChronoUnit.DAYS),"test");
+        Assertions.assertFalse(todoItem.isLate());
+    }
 
 }
